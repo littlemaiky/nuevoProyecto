c0af6dd02201e7e6709b6dc165ecced67d025d0e
AbnerZ0 <019201832d@uandina.edu.pe>
2022-10-16 12:43:55 -0500

    primer

---
+++ src/app/model/curso.ts
@@ -0,0 +1,11 @@
+import * as internal from "stream";
+
+export interface curso  {
+    id: string,
+    precio: string,
+    nombre: string,
+    autor: string,
+    descripcion: string,
+    imagen: string,
+    
+}
--- src/app/prueba13/prueba13.component.ts
+++ src/app/prueba13/prueba13.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { curso } from '../model/curso';
 
 @Component({
   selector: 'app-prueba13',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prueba13.component.css']
 })
 export class Prueba13Component implements OnInit {
+
+  cursos: curso[] = [
+
+    {
+    id: "01",
+    nombre: "MONALISA",
+    autor: "JUAN",
+    descripcion: "obra maestra",
+    imagen: ""
+    },
+
+  ];
 
   constructor() { }
 
